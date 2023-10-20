39a59ffb63025cf0f45871c5aa64c254bf59b673
Shreya Mathur <smathur245@gmail.com>
2023-10-20 23:12:37 +0530

    updated sponsors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,20 +1,36 @@
 import Devfolio from '../components/assets/sponsers/Devfolio.png'
 import Polygon from '../components/assets/sponsers/polygon_logo_white.png'
 import Replit from '../components/assets/sponsers/Replit.png'
+import Beeceptor from '../components/assets/sponsers/beeceptor_logo.png'
+import Wolfram from '../components/assets/sponsers/Wolfram_logo.png'
+import Rosenfeld from '../components/assets/sponsers/RosenFeld_logo.webp'
+import Stickermule from '../components/assets/sponsers/Stickermule_logo.png'
+import Echo3d from '../components/assets/sponsers/echo3D_logo.png'
+
 export default function Sponsors() {
-    return (
-        <div className="sponsors" id="sponsors">
-            <div className="heading">Sponsors</div>
-            <div className="temp-sponsors">Gold Sponsers</div>
-            <div className='images_sponsers'>
-             <a href="https://devfolio.co/"><img className='devfolio' id="gold" src={Devfolio} alt='DEVFOLIO LOGO'/></a> 
-               <a href="https://polygon.technology/"> <img className='devfolio' id="polygon" src={Polygon} alt='POLYGON LOGO'/></a>
-            </div>
-            <div className='silverSponsers'>Silver Sponsers</div>
-            <div className="silverImages">
-              <a href="https://replit.com/"><img className="devfolio" id="diff"  src={Replit} alt="REPLIT LOGO"></img></a>
-           
-            </div>
-        </div>
-    )
+  return (
+    <div className="sponsors" id="sponsors">
+      <div className="heading">Sponsors</div>
+      <div className="temp-sponsors">Gold Sponsers</div>
+      <div className='images_sponsers'>
+        <a href="https://devfolio.co/"><img className='devfolio' id="gold" src={Devfolio} alt='DEVFOLIO LOGO' /></a>
+        <a href="https://polygon.technology/"> <img className='devfolio' id="polygon" src={Polygon} alt='POLYGON LOGO' /></a>
+      </div>
+      <div className='silverSponsers'>Silver Sponsers</div>
+      <div className="silverImages">
+        <a href="https://replit.com/"><img className="silver" id="diff" src={Replit} alt="REPLIT LOGO"></img></a>
+        <a href="https://beeceptor.com/"><img className="silver" id="diff" src={Beeceptor} alt="BEECEPTOR LOGO"></img></a>
+        <a href="https://www.wolfram.com/"><img className="silver" id="diff" src={Wolfram} alt="WOLFRAM LOGO"></img></a>
+
+      </div>
+      <div className="silverImages">
+        <a href="https://rosenfeldmedia.com/"><img className="silver" id="diff" src={Rosenfeld} alt="ROSENFELD LOGO"></img></a>
+        <a href="https://www.stickermule.com/"><img className="silver" id="diff" src={Stickermule} alt="STICKERMULE LOGO"></img></a>
+        <a href="https://www.echo3d.com/"><img className="silver" id="diff" src={Echo3d} alt="ECHO3D LOGO"></img></a>
+
+
+      </div>
+
+    </div>
+  )
 }
