a982c9e598a368dc550b18eef84d6805b04bc4f1
Shivam <kalrashivam47@gmail.com>
2024-10-05 16:22:16 +0530

    Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Sponsors from './Components/Sponsors';
-import Tracks from './Components/Tracks';
-import About from './Components/About';
-import './App.css'; 
-import FAQ from './Components/FAQ';
-import Home from './Components/Home';
-import StarfieldBackground from './Components/StarfieldBackground'; // Ensure correct import
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Sponsors from "./Components/Sponsors";
+import Tracks from "./Components/Tracks";
+import About from "./Components/About";
+import "./App.css";
+import FAQ from "./Components/FAQ";
+import Home from "./Components/Home";
+import StarfieldBackground from "./Components/StarfieldBackground"; // Ensure correct import
 
 function App() {
   return (
     <div className="App">
-      <StarfieldBackground />
+      {/* <StarfieldBackground /> */}
       <Header />
       <main>
         <Home />
