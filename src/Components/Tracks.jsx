e5a2198198ab6ee9bebf5ea019b3231c51e6f57b
Shivam <kalrashivam47@gmail.com>
2024-10-06 11:48:58 +0530

    Tracks

--- src/Components/Tracks.jsx
+++ src/Components/Tracks.jsx
@@ -1,19 +1,12 @@
-import React from 'react';
-import './Tracks.css';
-import AnimatedTitle from './AnimatedTitle';
-
-// Import GIFs from the assets folder
-import blockchain1 from '../assets/blockchain1.gif';
-import cyber from '../assets/cyber.gif';
-import ai1 from '../assets/ai1.gif';
-import arvr1 from '../assets/arvr1.gif';
-import innovation from '../assets/innovation.gif';
+import React from "react";
+import "./Tracks.css";
+import AnimatedTitle from "./AnimatedTitle";
 
 function Tracks() {
   return (
-    <div id='Tracks'>
+    <div id="Tracks">
       <AnimatedTitle text="TRACKS" />
-      <div className="images">
+      {/* <div className="images">
         <div className="tracks">
           <img src={blockchain1} alt="Web3 and Blockchain" />
           <h2>Web3 and Blockchain</h2>
@@ -34,7 +27,7 @@ function Tracks() {
           <img src={innovation} alt="Open Innovation" />
           <h2>Open Innovation</h2>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
