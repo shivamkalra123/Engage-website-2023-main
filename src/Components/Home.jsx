b1a0e94b89ca8fc53d1c18be5cb5e4e3763cdf7f
Shivam <kalrashivam47@gmail.com>
2024-10-10 20:34:22 +0530

    DevfolioButton

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import './Home.css';
-import Countdown from './Countdown';
+import React from "react";
+import "./Home.css";
+import Countdown from "./Countdown";
+import DevfolioButton from "./DevfolioButton";
 
 const Home = () => {
-  const targetDate = 'Oct 21, 2024 23:59:59';
-  
+  const targetDate = "Oct 21, 2024 23:59:59";
+
   return (
     <div className="home">
       <section className="section">
         <div className="text-main-home">
-        <h1 className="main-title">Engage 4.0</h1>
-        <h3 className="tagline">Hack · Engage · Build</h3>
+          <h1 className="main-title">Engage 4.0</h1>
+          <h3 className="tagline">Hack · Engage · Build</h3>
         </div>
+        <DevfolioButton />
         <Countdown targetDate={targetDate} />
       </section>
     </div>
   );
-}
+};
 
 export default Home;
