b33ff06714cbd2a273cdf5171b81f9f24fd491c6
Shreya Mathur <72778961+Shreya2507@users.noreply.github.com>
2024-10-17 14:00:15 +0530

    Update Sponsors.jsx

--- src/Components/Sponsors.jsx
+++ src/Components/Sponsors.jsx
@@ -21,8 +21,8 @@ function Sponsors() {
       <AnimatedTitle text="SPONSORS" />
       <div className="sponsors-flex">
         
-        {/* <div className="sponsorship">
-          <h1 className='gold'>Gold Sponsors</h1>
+        <div className="sponsorship">
+          <h1 className='gold'>Platform Partners</h1>
           <div className="images-sponsors">
             <a href="https://devfolio.co/" target="_blank" rel="noopener noreferrer">
               <img src={devfolio} alt="Gold Sponsor 1" />
@@ -30,47 +30,22 @@ function Sponsors() {
             <a href="https://www.polygon.technology/" target="_blank" rel="noopener noreferrer">
               <img src={poly} alt="Gold Sponsor 2" />
             </a>
+            <a href="https://devfolio.co/blog/content/images/size/w600/2021/04/ethindia-1.jpg" target="_blank" rel="noopener noreferrer">
+              <img src={poly} alt="Gold Sponsor 3" />
+            </a>
           </div>
+          
         </div>
 
         <div className="sponsorship">
-          <h1 className='silver'>Silver Sponsors</h1>
+          <h1 className='silver'>Partners</h1>
           <div className="images-sponsors">
             <a href="https://www.wolfram.com/" target="_blank" rel="noopener noreferrer">
               <img src={woldram} alt="Silver Sponsor 1" />
             </a>
-            <a href="https://beeceptor.com/" target="_blank" rel="noopener noreferrer">
-              <img src={bee} alt="Silver Sponsor 2" />
-            </a>
-            <a href="https://www.stickermule.com/" target="_blank" rel="noopener noreferrer">
-              <img src={sticker} alt="Silver Sponsor 3" />
-            </a>
-            <a href="https://www.echo3d.com/" target="_blank" rel="noopener noreferrer">
-              <img src={echo} alt="Silver Sponsor 4" />
-            </a>
-            <a href="https://rosenfeldmedia.com/" target="_blank" rel="noopener noreferrer">
-              <img src={rosen} alt="Silver Sponsor 5" />
-            </a>
-            <a href="https://replit.com/" target="_blank" rel="noopener noreferrer">
-              <img src={replit} alt="Silver Sponsor 6" />
-            </a>
+            
           </div>
         </div>
-
-        <div className="sponsorship">
-          <h1 className='community'>Community Partners</h1>
-          <div className="images-sponsors">
-            <a href="https://gdsc.community.dev/institute-of-engineering-management-kolkata-india/" target="_blank" rel="noopener noreferrer">
-              <img src={gdsc1} alt="Community Partner 1" />
-            </a>
-            <a href="https://gdg.community.dev/gdg-gurugram/" target="_blank" rel="noopener noreferrer">
-              <img src={gdg} alt="Community Partner 2" />
-            </a>
-            <a href="https://gdsc.community.dev/netaji-subhas-university-of-technology-delhi-india/" target="_blank" rel="noopener noreferrer">
-              <img src={gdscncut} alt="Community Partner 3" />
-            </a>
-          </div>
-        </div> */}
 
 <h1 className='gold'>COMING SOON</h1>
 
